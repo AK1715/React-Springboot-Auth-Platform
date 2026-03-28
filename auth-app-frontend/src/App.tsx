eb8529ab38efc43b14e074c99c056714859cc019
AK1715 <ns031077@gmail.com>
2026-03-28 14:43:25 +0530

    wapper content

--- auth-app-frontend/src/App.tsx
+++ auth-app-frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import { Button } from "./components/ui/button"
+import FuturisticAuthHome from "./components/home/FuturisticAuthHome"
 
+ 
 export const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">
-        Hello auth frontend app!
-      </h1>
-      <Button variant={'destructive'}>Click me</Button>
+      <FuturisticAuthHome />
     </div>
-  )
+  );
 }
