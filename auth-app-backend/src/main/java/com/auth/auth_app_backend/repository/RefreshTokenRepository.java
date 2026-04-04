8b3c62775cfef9b8c32d2befd8af04f96c3e5863
AK1715 <ns031077@gmail.com>
2026-04-04 11:18:56 +0530

    created query for deletimng token by id

--- auth-app-backend/src/main/java/com/auth/auth_app_backend/repository/RefreshTokenRepository.java
+++ auth-app-backend/src/main/java/com/auth/auth_app_backend/repository/RefreshTokenRepository.java
@@ -1,6 +1,7 @@
 package com.auth.auth_app_backend.repository;
 
 import com.auth.auth_app_backend.entity.RefreshToken;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import java.util.UUID;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
     Optional<RefreshToken> findByJti(String jti);
-
+    @Transactional
+    void deleteByUserId(UUID userId);
 }
